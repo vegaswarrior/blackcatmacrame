3750ebcb2e442c38b0c8fc8b873d7f07775195cb
nicodeheza <nicolasdeheza@hotmail.com>
2022-06-21 16:04:29 -0300

    delete unnecessary import

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,6 @@
 import React, {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {Row, Col, Container, Button} from "react-bootstrap";
-import {Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Product from "../components/Product";
 import Message from "../components/Message";
@@ -34,36 +33,33 @@ const HomeScreen = ({match}) => {
 					Designing Beautiful Handmade Macrame Jewelry.
 				</h4>{" "}
 				<br />
-				
-				   <Button className="first_button" href="/collections">Discover More</Button>
-				
-				
+				<Button className="first_button" href="/collections">
+					Discover More
+				</Button>
 				<Col className="cat_col d-flex justify-content-between align-items-end">
-					<img src="../images/blackcat6.jpg" className="cat_image"  alt="black cata"></img>
+					<img src="../images/blackcat6.jpg" className="cat_image" alt="black cata"></img>
 					<img src="../images/blackcat5.jpg" className="cat_image" alt="black cata"></img>
 				</Col>
 			</Container>
 			<Container fluid className="featured_products_container">
-				
-					<Meta />
-					<h1 className="latest_products text-center">Latest Products</h1>
-					{loading ? (
-						<Loader />
-					) : error ? (
-						<Message variant="danger">{error}</Message>
-					) : (
-						<Container>
-								<Row>
-								{products.map((product) => (
+				<Meta />
+				<h1 className="latest_products text-center">Latest Products</h1>
+				{loading ? (
+					<Loader />
+				) : error ? (
+					<Message variant="danger">{error}</Message>
+				) : (
+					<Container>
+						<Row>
+							{products.map((product) => (
 								<Col key={product._id} sm={12} md={6} lg={4} xl={4}>
 									<Product product={product} />
 								</Col>
-								))}
-							</Row>
-							<Paginate pages={pages} page={page} keyword={keyword ? keyword : ""} />
-						</Container>
-					)}
-			
+							))}
+						</Row>
+						<Paginate pages={pages} page={page} keyword={keyword ? keyword : ""} />
+					</Container>
+				)}
 			</Container>
 			<Testimonials />
 		</>
